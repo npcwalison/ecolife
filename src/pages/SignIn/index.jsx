a2b84fdb7c1d55aa9e9ab3771d58ae4022f2e4c1
npcwalison <gwalisonls@gmail.com>
2023-02-26 23:56:07 -0300

    update

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,13 @@
+import {
+    View,
+    Text
+} from './styles';
+
+
+export default function SignIn(){
+    return(
+        <View>
+            <Text>SignIn</Text>
+        </View>
+    )
+}
---
+++ src/pages/SignUp/index.jsx
@@ -0,0 +1,13 @@
+import {
+    View,
+    Text
+} from './styles';
+
+
+export default function SignUp(){
+    return(
+        <View>
+            <Text>SignUp</Text>
+        </View>
+    )
+}
