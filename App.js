6ff429e1537b81ffb446af46fa8bbc9ecde78154
npcwalison <gwalisonla@gmail.com>
2023-02-28 14:17:10 -0300

    update

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+
+import { NavigationContainer } from '@react-navigation/native';
+
 import Auth from './src/routes/auth';
 
 export default function App() {
   return (
-    <>
+    <NavigationContainer>
       <StatusBar style="auto" />
       <Auth />
-    </>
+    </NavigationContainer>
   );
 }
--- src/routes/auth.jsx
+++ src/routes/auth.jsx
@@ -1,16 +1,14 @@
-import { NavigationContainer } from '@react-navigation/native';
 import StackRoutes from '../routes/stack.routes';
 import Home from '../pages/Home';
 
-const loginAuth = false;
 
 export default function Auth() {
+    const loading = false;
+    const loginAuth = false;
+
+
     return (
-        <NavigationContainer>
-            {
-                //the user is logged in? (yes/no)
-                loginAuth? <Home /> : <StackRoutes />
-            }
-        </NavigationContainer>
+        //the user is logged in? (yes/no)
+        loginAuth? <Home /> : <StackRoutes />
     )
 }
