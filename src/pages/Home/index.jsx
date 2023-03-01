19adcc74e111a32b5a99433dd5c689ee9b38b52a
npcwalison <gwalisonla@gmail.com>
2023-03-01 16:41:44 -0300

    update

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,20 +1,20 @@
-import { useContext } from 'react';
+//import { useContext } from 'react';
 import {
     View,
     Text,
     Button
 } from './styles';
 
-import { AuthContext } from '../../contexts/auth';
+//import { AuthContext } from '../../contexts/auth';
 
 
 export default function Home() {
 
-    const { user } = useContext(AuthContext)
+    //const { user } = useContext(AuthContext)
 
     return (
         <View>
-            <Text>{user.name}</Text>
+            <Text>Home</Text>
         </View>
     )
 }
