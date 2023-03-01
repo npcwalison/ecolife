9665e8ca0ce13ed11a1b07f0f21b2abc7d9a82dc
npcwalison <gwalisonla@gmail.com>
2023-03-01 14:20:06 -0300

    cadastro

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -3,6 +3,8 @@ import React, { createContext, useState } from "react";
 
 export const  AuthContext = createContext({});
 
+import { useNavigation } from "@react-navigation/native";
+
 import api from '../services/api';
 
 
@@ -11,10 +13,18 @@ function AuthProvider({ children }){
         name: "Matheus"
     });
 
+    const navigation = useNavigation();
+
     async function signUp(name, email, password){
         console.log(`${name}\n${email}\n${password}`)
         try{
+            const response = await api.post('/users', {
+                name: name,
+                email: email,
+                password: password
+            })
 
+            navigation.goBack()
         }catch(err){
             console.log(`Error Cadastro: ${err}`)
         }
