5c4bd0d0b652a419fa6c3fe8d48656c425664d4c
npcwalison <gwalisonla@gmail.com>
2023-03-02 16:31:44 -0300

    Card

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,14 @@
 //import { useContext } from 'react';
 import {
     Container,
-    Text
+    //card-attributes
+    Card,
+    Bank,
+    Chip,
+    CodeCard,
+    CardDataContainer,
+    CardData,
+    NameCard
 } from './styles';
 
 //import { AuthContext } from '../../contexts/auth';
@@ -13,7 +20,16 @@ export default function Home() {
 
     return (
         <Container>
-            <Text>Home</Text>
+            <Card>
+                <Bank>ECOLIFE</Bank>
+                <Chip></Chip>
+                <CodeCard>2344 2342 3242 2342</CodeCard>
+                <CardDataContainer>
+                    <CardData>564</CardData>
+                    <CardData>05/25</CardData>
+                </CardDataContainer>
+                <NameCard>Guilherme Santana</NameCard>
+            </Card>
         </Container>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,13 +1,54 @@
 import styled from "styled-components";
 
 
-export const Container = styled.KeyboardAvoidingView`
+export const Container = styled.View`
     flex: 1;
-    justify-content: center;
     align-items: center;
     background: #121222;
 `;
-
-export const Text = styled.Text`
+//CARD
+export const Card = styled.View`
+    width: 323.52px;
+    height: 204.01px;
+    border-radius: 8px;
+    margin-top: 20px;
+    padding: 10px 30px;
+    background-color: #f15104;
+`;
+export const Bank = styled.Text`
+    color: #fff;
+    font-weight: 800;
+`;
+export const Chip = styled.View`
+    width: 56.69px;
+    height: 45.35px;
+    background-color: #ff0;
+    border-radius: 8px;
+    margin-top: 35px;
+`;
+export const CodeCard = styled.Text`
+    font-size: 20px;
+    margin-top: 8px;
     color: #fff;
 `;
+
+export const CardDataContainer = styled.View`
+    color: #fff;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const CardData = styled.Text`
+    color: #fff;
+    font-size: 14px;
+    margin-top: 2px;
+`;
+export const NameCard = styled.Text`
+    color: #fff;
+    font-size: 18px;
+    margin-top: 2px;
+    text-transform: uppercase;
+`;
+//banner
+//trasações
